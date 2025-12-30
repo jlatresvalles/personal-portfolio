81d993980be4a1179162b9cf38136465c1ad6d3a
jlatresvalles <jlatresvalles@gmail.com>
2025-12-30 11:24:40 -0500

    Third commit, added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Experience } from "@/sections/Experience";
+import { Contact } from "@/sections/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Contact />
+      </main>
+    </div>
+      
   )
 }
 
