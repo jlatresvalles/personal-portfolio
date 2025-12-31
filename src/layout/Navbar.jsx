39af65347a110ab442d2144a5a0843c2626dcb17
jlatresvalles <jlatresvalles@gmail.com>
2025-12-31 08:31:11 -0500

    More changes to Navbar, added Button component

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/Button";
+
 const navLinks = [
     {href: "#about", label: "About"},
     {href: "#projects", label: "Projects"},
@@ -19,15 +21,24 @@ export const Navbar = () => {
 
                 {/* Desktop Nav */}
                 <div className="flex items-center gap-1">
-                    <div className="glass">
+                    <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
                         {navLinks.map((link, index) => (
                             <a 
                                 key={index} 
-                                href={link.href}>
+                                href={link.href}
+                                className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface"
+                            >
                                 {link.label}
                             </a>
                         ))}
                     </div>
+                </div>
+
+                {/* Call to Action Section */}
+                <div>
+                    <Button size="sm">
+                        <span className="relative flex items-center justify gap-2">Contact Me</span>
+                    </Button>
                 </div>
             </nav>
         </header>
